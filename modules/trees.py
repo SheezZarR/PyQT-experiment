f4f26ae829884524d2262c833610155a7d37bc86
Yaroslav (SheezZarR) Baslin <wertout77@gmail.com>
2022-05-17 18:52:02 +0300

    Array based tree is complete

--- modules/trees.py
+++ modules/trees.py
@@ -22,18 +22,48 @@ class ArrayBinTree:
         self._data.append(element)
 
     def insert_element(self, pos, element):
-        """ Inserts an element with a given position into the tree. """
+        """ Inserts an element at given position into the tree. """
+        arr_size = len(self._data)
 
         parent = pos // 2 - 1 + (pos // 2) % 2
+        left_of_pos = pos // 2 - 1 + (pos // 2) % 2
+        right_of_pos = pos // 2 - 1 + (pos // 2) % 2
+
+        left = self._data[left_of_pos] if left_of_pos > arr_size else None
+        right = self._data[right_of_pos] if right_of_pos > arr_size else None
 
         if parent < len(self._data):
-            if self._data[parent] and pos < len(self._data):
-                self._data[pos] = element
-            else:
-                self.resize(desired_size=pos)
-                self._data[pos] = element
+            if not left and not right:
+                if self._data[parent] and pos < len(self._data):
+                    self._data[pos] = element
+                else:
+                    self.resize(desired_size=pos)
+                    self._data[pos] = element
 
-        raise RuntimeError(f"Невозможно вставить элемент {element} на позицию {pos}. Нет родителя!")
+            raise IndexError(f"Невозможно вставить элемент {element} на позицию {pos}. Есть потомки!")
+
+        raise IndexError(f"Невозможно вставить элемент {element} на позицию {pos}. Нет родителя!")
+
+    def remove_element(self, pos):
+        """ Removes an element at given position from the tree. """
+
+        arr_size = len(self._data)
+
+        if len(self._data) <= pos:
+            raise IndexError(f"Невозможно удалить элемент на позиции {pos}!")
+
+        left = (pos * 2 + 1) if pos * 2 + 1 < arr_size else 'Нету'
+        right = (pos * 2 + 2) if pos * 2 + 2 < arr_size else 'Нету'
+
+        if right == 'Нету' and left == 'Нету':
+            raise RuntimeError(f"У удаляемого элемента есть потомок!")
+        else:
+            if left == 'Нету':
+                self._data[pos] = self._data[right]
+                self._data[right] = None
+            else:
+                self._data[pos] = self._data[left]
+                self._data[left] = None
 
     def show_tree(self) -> str:
         """ Returns a string representing the tree. """
@@ -82,10 +112,13 @@ class TreeNode:
 
 class LinkedListBinTree:
 
+    _top = None
 
+    def __init__(self):
+        pass
 
-def main():
-    tree = ArrayBinTree()
+    def add_element(self, elem):
+        pass
 
-
-main()
+    def show_tree(self, elem):
+        pass
